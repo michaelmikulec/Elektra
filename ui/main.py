e245e8dfeceac1008fa867a2ebdb4f468ecc7474
michaelmikulec <michaelmikulec1@gmail.com>
2025-03-25 19:28:02 -0400

    Update main.py

--- ui/main.py
+++ ui/main.py
@@ -3,9 +3,9 @@ from dash import Dash, html
 app = Dash(__name__)
 
 app.layout = html.Div(children=[
-  html.H1("Hello, Dash!"),
-  html.H2("This is a heading"),
-  html.P("This is a paragraph"),
+  html.H1("Elektra"),
+  html.H2("Harmful Brain Activity Classification"),
+
 ])
 
 if __name__ == '__main__':
