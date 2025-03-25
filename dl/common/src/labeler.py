3797e5da1d51054f27b4604142cf945e79aae6bb
michaelmikulec <michaelmikulec1@gmail.com>
2025-03-25 15:07:51 -0400

    updated labeler.py

--- dl/common/src/labeler.py
+++ dl/common/src/labeler.py
@@ -1,4 +1,5 @@
 import os
+import json
 import pandas as pd
 import time
 
@@ -6,70 +7,57 @@ def find_parquet_file(folder, file_id):
   file_path = os.path.join(folder, f"{file_id}.parquet")
   return file_path if os.path.exists(file_path) else None
 
-def extract_event_window(df, offset, samples_per_second, event_duration=50):
-  row_offset = offset * samples_per_second
-  event_rows = samples_per_second * event_duration
+def extract_event_window(df, seconds_offset, rows_per_second, event_duration):
+  row_offset = seconds_offset * rows_per_second
+  event_rows = rows_per_second * event_duration
   return df.iloc[row_offset : row_offset + event_rows]
 
-def label_data(
-  metadata_csv=None, 
-  eeg_folder=None, 
-  spec_folder=None, 
-  processed_eeg_folder=None, 
-  processed_spec_folder=None, 
-  samples_per_second=200, 
-  event_duration=50, 
-  label_eegs=False, 
-  label_specs=False
-):
-  metadata_df = pd.read_csv(metadata_csv)
+def label_data():
+  metadata = pd.read_csv("./data/train.csv")
+  unlabeled_eeg_dir = "./data/train_eegs/"
+  unlabeled_spec_dir = "./data/train_spectrograms/"
+  labeled_eeg_dir = "./data/labeled_train_eegs/"
+  labeled_spec_dir = "./data/labeled_train_specs/"
 
-  for index, row in metadata_df.iterrows():
-    eeg_id, eeg_sub_id, eeg_offset = row["eeg_id"], row["eeg_sub_id"], int(row["eeg_label_offset_seconds"])
-    spec_id, spec_sub_id, spec_offset = row["spectrogram_id"], row["spectrogram_sub_id"], int(row["spectrogram_label_offset_seconds"])
+  for index, row in metadata.iterrows():
+    eeg_id = row["eeg_id"],
+    eeg_sub_id = row["eeg_sub_id"],
+    eeg_offset = int(row["eeg_label_offset_seconds"]),
+    eeg_rows_per_second = 200,
+    eeg_event_duration = 50
+
+    spec_id  = row["spectrogram_id"],
+    spec_sub_id = row["spectrogram_sub_id"], 
+    spec_offset = int(row["spectrogram_label_offset_seconds"])
+    spec_rows_per_second = 0.5,
+    spec_event_duration = 600
+
     label = row["expert_consensus"]
-    label_index = {
-      "Seizure": 0,
-      "LRDA": 1, 
-      "GRDA": 2, 
-      "LPD": 3, 
-      "GPD": 4, 
-      "Other": 5
-    }    
+    label_index = { "Seizure": 0, "LRDA": 1, "GRDA": 2, "LPD": 3, "GPD": 4, "Other": 5 }    
     label_id = label_index[label]
 
-    eeg_file_path = find_parquet_file(eeg_folder, eeg_id)
-    if eeg_file_path and label_eegs:
+    eeg_file_path = find_parquet_file(unlabeled_eeg_dir, eeg_id)
+    if eeg_file_path:
       try:
         eeg_df = pd.read_parquet(eeg_file_path)
-        event_eeg = extract_event_window(eeg_df, eeg_offset, samples_per_second, event_duration)
-        eeg_output_path = os.path.join(processed_eeg_folder, f"{label_id}_{label}_{eeg_id}_{eeg_sub_id}.parquet")
-        event_eeg.to_parquet(eeg_output_path, index=False)
+        eeg_event_df = extract_event_window(eeg_df, eeg_offset, eeg_rows_per_second, eeg_event_duration)
+        eeg_output_path = os.path.join(labeled_eeg_dir, f"{label_id}_{label}_{eeg_id}_{eeg_sub_id}.parquet")
+        eeg_event_df.to_parquet(eeg_output_path, index=False)
       except:
         print(f"Error handling EEG {eeg_id}")
 
-    spec_file_path = find_parquet_file(spec_folder, spec_id)
-    if spec_file_path and label_specs:
+    spec_file_path = find_parquet_file(unlabeled_spec_dir, spec_id)
+    if spec_file_path:
       try:
         spec_df = pd.read_parquet(spec_file_path)
-        event_spec = extract_event_window(spec_df, spec_offset, samples_per_second, event_duration)
-        spec_output_path = os.path.join(processed_spec_folder, f"{label_id}_{label}_{spec_id}_{spec_sub_id}.parquet")
-        event_spec.to_parquet(spec_output_path, index=False)
+        spec_event_df = extract_event_window(spec_df, spec_offset, spec_rows_per_second, spec_event_duration)
+        spec_output_path = os.path.join(labeled_spec_dir, f"{label_id}_{label}_{spec_id}_{spec_sub_id}.parquet")
+        spec_event_df.to_parquet(spec_output_path, index=False)
       except:
         print(f"Error handling Spectrogram {spec_id}")
 
 if __name__ == "__main__":
   start = time.time()
-  label_data(
-    metadata_csv="./data/train.csv", 
-    eeg_folder="./data/train_eegs/", 
-    spec_folder="./data/train_spectrograms/", 
-    processed_eeg_folder="./data/labeled_eeg/", 
-    processed_spec_folder="./data/labeled_spec/", 
-    samples_per_second = 200,
-    event_duration = 50,
-    label_eegs=False,
-    label_specs=False
-  )
+  label_data()
   end = time.time()
   print(f"Labeling took {end - start} seconds")
