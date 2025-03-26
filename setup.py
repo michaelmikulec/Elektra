1296339ef3f5c85acb7f4d6227aed33b9537d5fc
michaelmikulec <michaelmikulec1@gmail.com>
2025-03-26 18:59:37 -0400

    enormous commit. just making the structure more professional

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="Elektra",
+  version="0.1.0",
+  description="Software application to classify harmful brain activities in EEG signals using deep learning and machine learning",
+  author="Michael Mikulec",
+  author_email="michaelmikulec1@gmail.com",
+  url="https://github.com/michaelmikulec/Elektra",
+  packages=find_packages(),
+  install_requires=[
+    "pandas",
+    "torch",
+    "pyarrow"
+  ],
+  classifiers=[
+    "Programming Language :: Python :: 3.13",
+    "Operating System :: OS Independent"
+  ],
+  python_requires=">=3.13.2"
+)
