e80f8f2b7218362c2148cdc82f80c17d6354d80b
michaelmikulec <michaelmikulec1@gmail.com>
2025-03-30 17:05:52 -0400

    added infer() to transformer.py

--- dl/transformer/inferencer.py
+++ dl/transformer/inferencer.py
@@ -3,22 +3,3 @@ from torch.nn import DataParallel
 from torch.cuda.amp import autocast
 from dl.transformer.archive.model import EEGTransformer
 
-def infer(inputs, model_path):
-  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  model = EEGTransformer(**model_config)
-
-  if torch.cuda.device_count() > 1:
-    model = DataParallel(model)
-
-  model.to(device)
-  model.load_state_dict(torch.load(model_path, map_location=device))
-  model.eval()
-
-  if inputs.ndim == 2:
-    inputs = inputs.unsqueeze(0)
-
-  with torch.no_grad():
-    inputs = inputs.to(device)
-    with autocast():
-      outputs = model(inputs)
-    return outputs
--- dl/transformer/tranformer.py
+++ dl/transformer/tranformer.py
@@ -153,6 +153,23 @@ def select_files_per_class(dataFiles, x):
   return result
 
 
+def infer(inputs, model_path):
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  model = EEGTransformer(**model_config)
+  if torch.cuda.device_count() > 1:
+    model = DataParallel(model)
+  model.to(device)
+  model.load_state_dict(torch.load(model_path, map_location=device))
+  model.eval()
+  if inputs.ndim == 2:
+    inputs = inputs.unsqueeze(0)
+  with torch.no_grad():
+    inputs = inputs.to(device)
+    with autocast():
+      outputs = model(inputs)
+    return outputs
+
+
 def train(modelName, modelDir,  dataDir, numPerClass, batchSize, lr, numEpochs):
   dataFiles = [
     os.path.join(dataDir, f)
