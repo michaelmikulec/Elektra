ba40868891919cea4c20fed5af7cc12d16b62cfc
Michael Phillip Mikulec <59239660+michaelmikulec@users.noreply.github.com>
2025-04-26 00:06:27 -0400

    Update tf.py

--- tf.py
+++ tf.py
@@ -7,21 +7,6 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader, random_split
 from preprocessor import df_to_spectrograms
 
-# class EEGDataset(Dataset):
-#   def __init__(self, folder, classes):
-#     self.files   = [f for f in os.listdir(folder) if f.endswith('.parquet')]
-#     self.folder  = folder
-#     self.cls2idx = {c:i for i,c in enumerate(classes)}
-#   def __len__(self):
-#     return len(self.files)
-#   def __getitem__(self, idx):
-#     fn        = self.files[idx]
-#     label_str = fn.split('_')[-1].split('.')[0]
-#     label_idx = self.cls2idx[label_str]
-#     df        = pd.read_parquet(os.path.join(self.folder, fn))
-#     data      = torch.from_numpy(df.values).float()
-#     return data, torch.tensor(label_idx).long()
-
 class EEGTransformer(nn.Module):
   def __init__(
     self,
